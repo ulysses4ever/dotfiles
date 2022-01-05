7e03f98c27f59c9697d10f3eecbf595b092639f1
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-01-05 17:41:56 -0500

    Doom Emacs: setup roam dir, emoji

--- doom.d/config.el
+++ doom.d/config.el
@@ -21,7 +21,8 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "Fira Code" :size 14 :weight 'medium))
+(setq doom-font (font-spec :family "monospace" :size 14 :weight 'medium)
+      doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -30,7 +31,8 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Dropbox/Notes")
+(setq org-directory "~/Dropbox/Notes/roam")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
