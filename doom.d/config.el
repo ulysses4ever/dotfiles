b7f2caef17fe4b26992fefc53d7dee0f85b1bd2a
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-02-20 22:49:05 -0500

    doom: roam stuff

--- doom.d/config.el
+++ doom.d/config.el
@@ -34,6 +34,12 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Dropbox/Notes")
+(setq org-roam-directory "~/Dropbox/Notes/memos")
+(setq org-roam-capture-templates
+      '(("d" "default" plain "%?"
+         :target (file+head "%<%Y-%m-%d>-${slug}.org"
+                            "#+title: ${title}\n")
+         :unnarrowed t)))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
