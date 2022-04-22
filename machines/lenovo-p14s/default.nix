5c09a50e352f5445de31d8154b0049ff8eb5e1be
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-04-22 19:58:23 -0400

    add VPN service

--- machines/lenovo-p14s/default.nix
+++ machines/lenovo-p14s/default.nix
@@ -208,6 +208,8 @@ in
     };
   };
 
+  services.globalprotect.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.artem = {
     isNormalUser = true;
