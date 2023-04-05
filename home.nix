9bc16bf3024e23eafc755161779cdc0e16454ff0
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-04-05 09:54:57 -0400

    add minimal vscode config

--- home.nix
+++ home.nix
@@ -25,6 +25,24 @@
 
   home.file.".ghci".source = ./.ghci;
 
+  # vscode
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      dracula-theme.theme-dracula
+      vscodevim.vim
+      yzhang.markdown-all-in-one
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "language-julia";
+        publisher = "julialang";
+        version = "1.45.1";
+        sha256 = "PC+f3mEaiHMYvS5nUmB8e4596Cx2PCBfxKbrE8tFLa4=";
+        #         1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g
+      }
+    ];
+  };
+
   # foot
   programs.foot = {
     enable = true;
