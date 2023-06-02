0c5b1bb94315abe0d14bb816b4d548c8ab4e1b36
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-06-02 09:30:15 -0400

    hm-only: emacs, doom, and their fonts

--- hm-only/machines/hp-julia/default.nix
+++ hm-only/machines/hp-julia/default.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 {
   imports = [ ../../../home.nix];
-  home.packages = (import ../../../packages.nix) pkgs ++ [ pkgs.fira-code ];
+  home.packages = (import ../../../packages.nix) pkgs ++ [ pkgs.fira-code pkgs.roboto ];
+
+  # Emacs
+  programs.emacs.enable = true;
+  home.file.".doom.d".source = ../../../doom.d;
 }
