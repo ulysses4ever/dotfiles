900b2e51b17a3e8f4b871d389c269ff0156718ab
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-06-02 09:28:02 -0400

    emacs: there's no "serif" font in Ubuntu for some reason, so take Roboto

--- doom.d/config.el
+++ doom.d/config.el
@@ -24,7 +24,7 @@
 (setq doom-font
         (font-spec :family "Fira Code" :size 14 :weight 'medium)
       doom-variable-pitch-font
-        (font-spec :family "serif" :size 13))
+        (font-spec :family "Roboto" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
