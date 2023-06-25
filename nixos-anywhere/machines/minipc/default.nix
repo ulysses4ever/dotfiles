e9e68691f199be804c7a256fdd2b2a91817b5d77
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-06-24 20:40:14 -0400

    nixos-anywhere: fix networking

--- nixos-anywhere/machines/minipc/default.nix
+++ nixos-anywhere/machines/minipc/default.nix
@@ -9,10 +9,10 @@
     networkmanager.enable = true;
     firewall.enable = false;
 
-    # Announce myself as $hostname.local in a local network and help others to do the same
-    # https://github.com/NixOS/nixpkgs/issues/98050#issuecomment-1471678276
-    services.resolved.enable = true;
-    networking.networkmanager.connectionConfig."connection.mdns" = 2;
-    services.avahi.enable = true;
   };
+  # Announce myself as $hostname.local in a local network and help others to do the same
+  # https://github.com/NixOS/nixpkgs/issues/98050#issuecomment-1471678276
+  services.resolved.enable = true;
+  networking.networkmanager.connectionConfig."connection.mdns" = 2;
+  services.avahi.enable = true;
 }
