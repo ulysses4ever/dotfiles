28e51d362c50fd7ef210544c1df1fea92ecc3889
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-11-01 16:18:41 -0400

    netcup: some nginx stuff for serving home dir; also nvim

--- nixos-anywhere/machines/netcup/default.nix
+++ nixos-anywhere/machines/netcup/default.nix
@@ -19,7 +19,39 @@
       root = "/var/www";
     };
   };
+
+  # Artem, 2023-11-01:
+  # I failed to figure out how to serve $USER/public_html proper
+  users.users.artem.homeMode = "750";
+  users.users.artem.extraGroups = [ "nginx" ];
+  users.groups."nginx" = {};
+#  services.nginx.virtualHosts."web.pelenitsyn.top".locations = {
+#    "~ ^/~artem(/.*)?$" = {
+#      alias = "/var/www/artem$1";
+#      extraConfig = ''
+#        autoindex on;
+#        index     index.html;
+#      '';
+#    };
+#  };
+#  systemd.services.nginx.serviceConfig.ProtectHome = lib.mkForce false;
+#  systemd.services.nginx.serviceConfig.ProtectSystem = lib.mkForce false;
+#  systemd.services.nginx.serviceConfig.ReadOnlyPaths = [ "/home/" ];
+#       locations."~ ^/~(.+?)(/.*)?$" = {
+#         alias = "/home/$1/public_html$2;";
+#       };
+##############################################
+# end of nginx serving $USER/public_html experiments
+
   security.acme.certs = {
     "web.pelenitsyn.top".email = "a@pelenitsyn.top";
+  };
+
+  # basic comforts
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    defaultEditor = true;
   };
 }
