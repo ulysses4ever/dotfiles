d3ef52695e739cdfae6fdf1e48dde485529dc0c1
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2024-05-25 22:13:24 -0400

    flake: add newer SSH key to artem

--- flake.nix
+++ flake.nix
@@ -71,11 +71,17 @@
             users.groups.artem = {};
             users.users = {
               root = {
-                openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDipD0jpN1WxGOp+Ij5aqpLafM/6hCkf9ltaMpIlaBHxvNH2HRUbf5WOK8Vjb6lpHC0DZrsOgCc/FM96bGeIBmLZit9r1S6soAEHKIhHPjFhleBJo+T/b8F+Rm+afWMtUtVysQHe0u168g+NEovv0XzaDjBkZ+vOJUYL/u7YJbHDsLk1u+IzlIqCvelDYPrnJz49o849T3A3hfBlWx/q2WAsM8a6Wz2j+2ggzi8vo2RFQGzxswCq9KGO69XQjWgH5rw7d8I9jD2ccbj+mheVJuZLuYohTIkW8+i/93ReMvqate1LDIEpyQdm6OiyCVUn89BMmN186tNc+R5tvOaXFMT ulysses@ulysses-laptop-2" ];
+                openssh.authorizedKeys.keys = [
+                    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDipD0jpN1WxGOp+Ij5aqpLafM/6hCkf9ltaMpIlaBHxvNH2HRUbf5WOK8Vjb6lpHC0DZrsOgCc/FM96bGeIBmLZit9r1S6soAEHKIhHPjFhleBJo+T/b8F+Rm+afWMtUtVysQHe0u168g+NEovv0XzaDjBkZ+vOJUYL/u7YJbHDsLk1u+IzlIqCvelDYPrnJz49o849T3A3hfBlWx/q2WAsM8a6Wz2j+2ggzi8vo2RFQGzxswCq9KGO69XQjWgH5rw7d8I9jD2ccbj+mheVJuZLuYohTIkW8+i/93ReMvqate1LDIEpyQdm6OiyCVUn89BMmN186tNc+R5tvOaXFMT ulysses@ulysses-laptop-2"
+                    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9ROUiZuXiBhaQ46UK9Oj3aOWp57q/u3grK74KIZnreP+ehLj1e03RETMlPRgmafjaE8McNJt/CNx++T0uhtT2by+FgW13pb6s97RBTcb3Y4LCQK5SoW+CrtMXNJAbag1X+2YhpA/LYhsvzbQKMQ2RnEBkR2c8ftHpBkr0V+qA+9j3IiYxcv4dek9BWLmByRmrpYdMX8tB71BNbj4dEY40PkP3A8vxRHYwe1AQ3BJU664dtJcBCRXupYrLl+OWLx3wYrpe5n1gcqnh1AVnAoL+M79Hn1+egD28TRq5yGr5C3PzZv4bJpdLiIlEzuzBbVP/hzg3C0YziaBqqBwCn9orBnS66eQq8z4MU4kGa2DwDQ/M/4VOrpSnwjmmw7C8P/qLu6nlnyqwCFul/oRXUH0rmkBV6fPxBgw6FYPsJhHMlE+4j8q/laKUTmbrGKio13xYEdFUXT1BHICMS2N7JNKo/0HXR2Frj8XsV/d3ld0xzl3Gy9BNZUbOl/xL/gkNaXM= artem@lenovo-p14s"
+                    ];
                 initialHashedPassword = "$6$ij6xuG830qEsJmw4$LoYxxNdKIupMp/.ZP3oEn/EPAhVmbwsUAogfd7IIXZAR4SoLO3b8N2uTQK/2D83oVwjBRZdhbgoSVH/YEKRjU/";
               };
               artem = {
-                openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDipD0jpN1WxGOp+Ij5aqpLafM/6hCkf9ltaMpIlaBHxvNH2HRUbf5WOK8Vjb6lpHC0DZrsOgCc/FM96bGeIBmLZit9r1S6soAEHKIhHPjFhleBJo+T/b8F+Rm+afWMtUtVysQHe0u168g+NEovv0XzaDjBkZ+vOJUYL/u7YJbHDsLk1u+IzlIqCvelDYPrnJz49o849T3A3hfBlWx/q2WAsM8a6Wz2j+2ggzi8vo2RFQGzxswCq9KGO69XQjWgH5rw7d8I9jD2ccbj+mheVJuZLuYohTIkW8+i/93ReMvqate1LDIEpyQdm6OiyCVUn89BMmN186tNc+R5tvOaXFMT ulysses@ulysses-laptop-2" ];
+                openssh.authorizedKeys.keys = [
+                    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDipD0jpN1WxGOp+Ij5aqpLafM/6hCkf9ltaMpIlaBHxvNH2HRUbf5WOK8Vjb6lpHC0DZrsOgCc/FM96bGeIBmLZit9r1S6soAEHKIhHPjFhleBJo+T/b8F+Rm+afWMtUtVysQHe0u168g+NEovv0XzaDjBkZ+vOJUYL/u7YJbHDsLk1u+IzlIqCvelDYPrnJz49o849T3A3hfBlWx/q2WAsM8a6Wz2j+2ggzi8vo2RFQGzxswCq9KGO69XQjWgH5rw7d8I9jD2ccbj+mheVJuZLuYohTIkW8+i/93ReMvqate1LDIEpyQdm6OiyCVUn89BMmN186tNc+R5tvOaXFMT ulysses@ulysses-laptop-2"
+                    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9ROUiZuXiBhaQ46UK9Oj3aOWp57q/u3grK74KIZnreP+ehLj1e03RETMlPRgmafjaE8McNJt/CNx++T0uhtT2by+FgW13pb6s97RBTcb3Y4LCQK5SoW+CrtMXNJAbag1X+2YhpA/LYhsvzbQKMQ2RnEBkR2c8ftHpBkr0V+qA+9j3IiYxcv4dek9BWLmByRmrpYdMX8tB71BNbj4dEY40PkP3A8vxRHYwe1AQ3BJU664dtJcBCRXupYrLl+OWLx3wYrpe5n1gcqnh1AVnAoL+M79Hn1+egD28TRq5yGr5C3PzZv4bJpdLiIlEzuzBbVP/hzg3C0YziaBqqBwCn9orBnS66eQq8z4MU4kGa2DwDQ/M/4VOrpSnwjmmw7C8P/qLu6nlnyqwCFul/oRXUH0rmkBV6fPxBgw6FYPsJhHMlE+4j8q/laKUTmbrGKio13xYEdFUXT1BHICMS2N7JNKo/0HXR2Frj8XsV/d3ld0xzl3Gy9BNZUbOl/xL/gkNaXM= artem@lenovo-p14s"
+                    ];
                 initialHashedPassword = "$6$ij6xuG830qEsJmw4$LoYxxNdKIupMp/.ZP3oEn/EPAhVmbwsUAogfd7IIXZAR4SoLO3b8N2uTQK/2D83oVwjBRZdhbgoSVH/YEKRjU/";
                 isNormalUser = true;
                 createHome = true;
