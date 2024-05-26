b7f9379a6cb6572fc4bdfcbaf38f46f6228cb282
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2024-05-25 22:14:03 -0400

    um690: syncthing

--- machines/um690/default.nix
+++ machines/um690/default.nix
@@ -114,6 +114,33 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
+  services = {
+    syncthing = {
+      enable = true;
+      user = "artem";
+      overrideDevices = true;
+      overrideFolders = true;
+      configDir = "/home/artem/.config/syncthing";
+      settings = {
+        devices = {
+          "netcup" = { id = "U25H2L7-KJY7IXJ-HHD2D76-4LBMTBZ-2CJE2NM-DBZO2V7-IUVFGPI-JGBXRQA"; };
+          "pixel7a" = { id = "B2UK2TS-WJQ224N-MZ6UUSL-AHRZ6Z5-VMJWTFV-KZGWIJD-T66PZAS-OFPHUA2"; };
+        };
+        folders = {
+          "Dropbox" = {
+            path = "/home/artem/Dropbox";
+            devices = [ "netcup" "pixel7a" ];
+          };
+          "Pixel7a-Pictures" = {
+            id = "pixel_7a_j24u-photos";
+            path = "/home/artem/Pictures/Cell/pixel7a";
+            devices = [ "pixel7a" ];
+          };
+        };
+      };
+    };
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
