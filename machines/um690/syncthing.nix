257760740d783da6827c6c7a9237d8202763bcab
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2025-07-01 13:10:51 -0400

    um690: syncthing: add hp-julia

--- machines/um690/syncthing.nix
+++ machines/um690/syncthing.nix
@@ -13,11 +13,12 @@
           "netcup" = { id = "U25H2L7-KJY7IXJ-HHD2D76-4LBMTBZ-2CJE2NM-DBZO2V7-IUVFGPI-JGBXRQA"; };
           "pixel7a" = { id = "B2UK2TS-WJQ224N-MZ6UUSL-AHRZ6Z5-VMJWTFV-KZGWIJD-T66PZAS-OFPHUA2"; };
           "pixel7a-julia" = { id = "LRW6ASR-XLL2XS6-Z47AFJ4-BRM5SAP-ZV3BV4T-LQDN3MC-C56TFIV-3M3BMQR"; };
+          "hp-julia" = { id = "PEG4MSE-XPCGWKW-KU2NAGX-3LVZE4M-5IXN35U-BXWYPDZ-SGAXFKT-S4YQTA6"; };
         };
         folders = {
           "Dropbox" = {
             path = "/home/artem/Dropbox";
-            devices = [ "netcup" "pixel7a" ];
+            devices = [ "netcup" "pixel7a" "hp-julia" ];
           };
           "Pixel7a-Pictures" = {
             id = "pixel_7a_j24u-photos";
