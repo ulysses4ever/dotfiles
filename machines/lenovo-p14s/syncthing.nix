84071b02b935225ac86c707297abbd334e4bd383
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2025-07-01 15:28:19 -0400

    syncthing: rip netcup

--- machines/lenovo-p14s/syncthing.nix
+++ machines/lenovo-p14s/syncthing.nix
@@ -10,13 +10,12 @@
       configDir = "/home/artem/.config/syncthing";
       settings = {
         devices = {
-          "netcup" = { id = "U25H2L7-KJY7IXJ-HHD2D76-4LBMTBZ-2CJE2NM-DBZO2V7-IUVFGPI-JGBXRQA"; };
           "pixel7a" = { id = "B2UK2TS-WJQ224N-MZ6UUSL-AHRZ6Z5-VMJWTFV-KZGWIJD-T66PZAS-OFPHUA2"; };
         };
         folders = {
           "Dropbox" = {
             path = "/home/artem/Dropbox";
-            devices = [ "netcup" "pixel7a" ];
+            devices = [ "pixel7a" ];
           };
         };
       };
--- machines/um690/syncthing.nix
+++ machines/um690/syncthing.nix
@@ -10,7 +10,6 @@
       configDir = "/home/artem/.config/syncthing";
       settings = {
         devices = {
-          "netcup" = { id = "U25H2L7-KJY7IXJ-HHD2D76-4LBMTBZ-2CJE2NM-DBZO2V7-IUVFGPI-JGBXRQA"; };
           "pixel7a" = { id = "B2UK2TS-WJQ224N-MZ6UUSL-AHRZ6Z5-VMJWTFV-KZGWIJD-T66PZAS-OFPHUA2"; };
           "pixel7a-julia" = { id = "LRW6ASR-XLL2XS6-Z47AFJ4-BRM5SAP-ZV3BV4T-LQDN3MC-C56TFIV-3M3BMQR"; };
           "hp-julia" = { id = "KVNEPPV-XFAYHIS-BMPKUZG-I2TRIPW-5ZUGNO4-2YN7BTQ-CGHAVKE-XQE6NA7"; };
@@ -18,7 +17,7 @@
         folders = {
           "Dropbox" = {
             path = "/home/artem/Dropbox";
-            devices = [ "netcup" "pixel7a" "hp-julia" ];
+            devices = [ "pixel7a" "hp-julia" ];
           };
           "Pixel7a-Pictures" = {
             id = "pixel_7a_j24u-photos";
