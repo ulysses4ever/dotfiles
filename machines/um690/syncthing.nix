a5dd3b6294eb163169a4c935248f062e49f90e34
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2025-07-01 13:26:29 -0400

    correction to the previous commit

--- machines/um690/syncthing.nix
+++ machines/um690/syncthing.nix
@@ -13,7 +13,7 @@
           "netcup" = { id = "U25H2L7-KJY7IXJ-HHD2D76-4LBMTBZ-2CJE2NM-DBZO2V7-IUVFGPI-JGBXRQA"; };
           "pixel7a" = { id = "B2UK2TS-WJQ224N-MZ6UUSL-AHRZ6Z5-VMJWTFV-KZGWIJD-T66PZAS-OFPHUA2"; };
           "pixel7a-julia" = { id = "LRW6ASR-XLL2XS6-Z47AFJ4-BRM5SAP-ZV3BV4T-LQDN3MC-C56TFIV-3M3BMQR"; };
-          "hp-julia" = { id = "PEG4MSE-XPCGWKW-KU2NAGX-3LVZE4M-5IXN35U-BXWYPDZ-SGAXFKT-S4YQTA6"; };
+          "hp-julia" = { id = "KVNEPPV-XFAYHIS-BMPKUZG-I2TRIPW-5ZUGNO4-2YN7BTQ-CGHAVKE-XQE6NA7"; };
         };
         folders = {
           "Dropbox" = {
