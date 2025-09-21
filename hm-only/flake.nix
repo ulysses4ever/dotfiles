2fd68616b60f87170c09d77aa77da06bc944df5c
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2025-09-20 21:24:22 -0400

    hm-only: switch to unstable

--- hm-only/flake.nix
+++ hm-only/flake.nix
@@ -4,7 +4,7 @@
   description = "Standalone (Non-NixOS) Home Manager configuration of Artem";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
