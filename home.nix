b69578c949824dbbe759369935521e2f2ca1702a
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2026-01-09 09:55:32 -0500

    home.nix: adjust to options changes in 25.11

--- home.nix
+++ home.nix
@@ -186,22 +186,26 @@
     };
   };
 
+  # delta makes terminal diffs prettier
+  programs.delta = {
+    enable = true;
+    options = {
+      navigate = true;
+      line-numbers = true;
+      side-by-side = true;
+      theme = "GitHub";
+    };
+  };
+
   # Git
   programs.git = {
     enable = true;
-    userName  = "Artem Pelenitsyn";
-    userEmail = "a.pelenitsyn@gmail.com";
     signing.key = "A31C1BFA09B1F47D";
-    delta = {
-      enable = true;
-      options = {
-        navigate = true;
-        line-numbers = true;
-        side-by-side = true;
-        theme = "GitHub";
+    settings = {
+      user = {
+        name  = "Artem Pelenitsyn";
+        email = "a.pelenitsyn@gmail.com";
       };
-    };
-    extraConfig = {
       branch = { autosetuprebase = "always"; };
       core   = { editor = "vi"; };
       merge  = { conflictstyle = "diff3"; };
@@ -209,27 +213,27 @@
       init   = { defaultBranch = "main"; };
       blame  = { ignoreRevsFile = ".git-blame-ignore-revs"; };
       gpg    = { format = "ssh"; };
-    };
-    aliases = {
-      aa   = "add --all";
-      cam  = "commit -am";
-      cn   = "commit --amend";
-      cnn  = "commit --amend --no-edit";
-      cann = "commit -a --amend --no-edit";
-      c    = "clone";
-      co   = "checkout";
-      df   = "diff -w --color-words=.";
-      hist = "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short";
-      l    = "log";
-      l1   = "log --oneline";
-      lgb  = "log --graph --pretty=oneline --abbrev-commit --branches";
-      ph   = "push";
-      pl   = "pull";
-      rh   = "reset --hard";
-      s    = "status";
-      st   = "stash";
-      stp  = "stash pop";
-      sta  = "stash apply";
+      aliases = {
+        aa   = "add --all";
+        cam  = "commit -am";
+        cn   = "commit --amend";
+        cnn  = "commit --amend --no-edit";
+        cann = "commit -a --amend --no-edit";
+        c    = "clone";
+        co   = "checkout";
+        df   = "diff -w --color-words=.";
+        hist = "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short";
+        l    = "log";
+        l1   = "log --oneline";
+        lgb  = "log --graph --pretty=oneline --abbrev-commit --branches";
+        ph   = "push";
+        pl   = "pull";
+        rh   = "reset --hard";
+        s    = "status";
+        st   = "stash";
+        stp  = "stash pop";
+        sta  = "stash apply";
+      };
     };
   };
 
