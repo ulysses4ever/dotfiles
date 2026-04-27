1be4dada613c5219994801a9f83ae44f37101da5
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2026-04-27 11:22:38 -0400

    packages: light cleanup

--- packages.nix
+++ packages.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 
-{ environment.systemPackages = 
+{ environment.systemPackages =
 with pkgs; [
     # Command-Line Tools
     wget mc file bc
@@ -40,104 +40,102 @@ with pkgs; [
     mermaid-cli # diagrams from text
 
     # Gnome Desktop apps
-    nautilus
-    gedit
-    gnome-terminal
-    eog
-    evince
-    gnome-tweaks
-    dconf-editor
-    file-roller
-    gnomeExtensions.appindicator
-    gnomeExtensions.dash-to-dock
-    gnome-keyring
-    transmission_4-gtk
-    baobab
+     nautilus
+     gedit
+     gnome-terminal
+     eog
+     evince
+     gnome-tweaks
+     dconf-editor
+     file-roller
+     gnomeExtensions.appindicator
+     gnomeExtensions.dash-to-dock
+     gnome-keyring
+     transmission_4-gtk
+     baobab
 
-    # Text
+     # Text
 
-    #emacs # -- using Doom Emacs through Home Manager # -- not anymore
-    #emacsPgtkGcc
-    libtool libvterm # emacs vterm
-    irony-server
-    emacs-all-the-icons-fonts
-    copilot-language-server
+     libtool libvterm # emacs vterm
+     irony-server
+     emacs-all-the-icons-fonts
+     copilot-language-server
 
-    geany
-    (texlive.combine {
-        inherit (texlive)
-            scheme-medium
-            collection-langcyrillic
-            collection-latex
-            collection-latexrecommended
-            collection-latexextra
-            collection-fontsextra
-            collection-fontutils
-            collection-fontsrecommended
-            collection-publishers
-        ;
-    })
-    pplatex
-    biber
-    ott
-    libreoffice-fresh
-    aspell
-    aspellDicts.en enchant # helps with spell-checking in e.g. gEdit
+     geany
+     # (texlive.combine {
+     #     inherit (texlive)
+     #         scheme-medium
+     #         collection-langcyrillic
+     #         collection-latex
+     #         collection-latexrecommended
+     #         collection-latexextra
+     #         collection-fontsextra
+     #         collection-fontutils
+     #         collection-fontsrecommended
+     #         collection-publishers
+     #     ;
+     # })
+     # pplatex
+     # biber
+     # ott
+     # libreoffice-fresh
+     aspell
+     aspellDicts.en enchant # helps with spell-checking in e.g. gEdit
 
-    # Desktop
-    xdg-utils
-    firefox chromium
-    zoom-us
-    telegram-desktop
-    steam
-    shotwell
-#    bluejeans-gui
+     # Desktop
+     xdg-utils
+     firefox chromium
+     zoom-us
+     telegram-desktop
+     # steam
+     shotwell
+ #    bluejeans-gui
 
-    # A/V
-    pavucontrol
-    ffmpeg
-    vlc # don't support pure Wayland
-    mpv # do support it
-    #clapper # new kid on the block: supports Wayland but unpolished/buggy
-    droidcam
-    pamixer
-    mesa-demos
-    obs-studio obs-studio-plugins.wlrobs
-    wf-recorder v4l-utils
+     # A/V
+     pavucontrol
+     ffmpeg
+     vlc # don't support pure Wayland
+     mpv # do support it
+     #clapper # new kid on the block: supports Wayland but unpolished/buggy
+     droidcam
+     pamixer
+     mesa-demos
+     # obs-studio obs-studio-plugins.wlrobs
+     wf-recorder v4l-utils
 
-    # Develpoment
-    binutils gnumake gdb
-    nodejs # neovim wants it
+     # Develpoment
+     binutils gnumake gdb
+     nodejs # neovim wants it
 
-    ghc cabal-install ghcid
-    haskellPackages.alex
-    haskellPackages.happy
-    haskellPackages.hasktags # (used by Emacs)
-    haskell-language-server
+     ghc cabal-install ghcid
+     haskellPackages.alex
+     haskellPackages.happy
+     # haskellPackages.hasktags # (used by Emacs)
+     haskell-language-server
 
-#    coq_8_12
-#    (agda.withPackages (p: [p.standard-library]))
+ #    coq_8_12
+ #    (agda.withPackages (p: [p.standard-library]))
 
-    gcc git cmake
-    jdk ant maven
-    python3Minimal
-    julia-lts
+     gcc git cmake
+     jdk ant maven
+     python3Minimal
+     julia-lts
 
-    # direnv
+     # direnv
 
-    # *** Experimental ***
+     # *** Experimental ***
 
-    # Lighter Desktop apps
-    # spaceFM # file manager
+     # Lighter Desktop apps
+     # spaceFM # file manager
 
-    # styling for gtk apps
-    #lxappearance # don't work on pure Wayland, instead:
-    xfce.xfce4-settings # use xfce4-appearance-settings from here
+     # styling for gtk apps
+     #lxappearance # don't work on pure Wayland, instead:
+     xfce.xfce4-settings # use xfce4-appearance-settings from here
 
-    # NVIDIA+Wayland experiments
-    #egl-wayland
-    #xorg.libxcb
-    #mesa
-    #libglvnd
-    #libdrm
+     # NVIDIA+Wayland experiments
+     #egl-wayland
+     #xorg.libxcb
+     #mesa
+     #libglvnd
+     #libdrm
 ]; }
